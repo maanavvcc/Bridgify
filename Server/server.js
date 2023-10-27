8f633f950062ea48302fcff0e0e0a9a3a01ee052
Manav M. <kookygavinquack@gmail.com>
2023-10-27 04:30:44 -0400

    Changed Readme and added disconnect

--- Server/server.js
+++ Server/server.js
@@ -10,18 +10,14 @@ const connections = {};
 const cors = require('cors');
 app.use(cors());
 
-
+// Static Serves (Mobile Serves)
 app.use(express.static('public'));
 
 app.get('/connections', (req, res) => {
   res.json(connections);
 });
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../', 'WebApp', 'src'));
-// });
-
-app.use('/', express.static(path.join(__dirname, '../', 'WebApp', 'src')))
+app.use('/', express.static(path.join(__dirname, '../', 'WebApp', 'src')));
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
@@ -44,24 +40,59 @@ io.on('connection', (socket) => {
       socket.emit('connection-error', 'Invalid key or desktop already connected');
     }
   });
-  
-  
 
   // Handle disconnection
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
     for (const key in connections) {
-      if (connections[key].desktop === socket.id || connections[key].mobile === socket.id) {
-        delete connections[key];
+      if (connections[key].desktop === socket.id) {
+        if (connections[key].mobile) {
+          io.to(connections[key].mobile).emit('desktop-disconnected');
+          delete connections[key].mobile; // Removing mobile from connections as it is disconnected
+        }
+        delete connections[key]; // Removing the connection as desktop is disconnected
+      } else if (connections[key].mobile === socket.id) {
+        if (connections[key].desktop) {
+          io.to(connections[key].desktop).emit('mobile-disconnected');
+          delete connections[key].mobile; // Removing mobile from connections as it is disconnected
+        }
       }
     }
   });
+
+  socket.on('reset-connection', () => {
+    for (const key in connections) {
+      if (connections[key].desktop === socket.id) {
+        if (connections[key].mobile) {
+          io.to(connections[key].mobile).emit('desktop-disconnected');
+          delete connections[key].mobile; // Removing mobile from connections as desktop is resetting connection
+        }
+        const newKey = generateKey();
+        connections[newKey] = { desktop: socket.id, mobile: null };
+        socket.emit('key', newKey);
+      }
+    }
+  });
+
+  socket.on('mobile-disconnect', () => {
+    console.log('Mobile disconnected:', socket.id);
+    for (const key in connections) {
+      if (connections[key].mobile === socket.id) {
+        if (connections[key].desktop) {
+          io.to(connections[key].desktop).emit('mobile-disconnected');
+        }
+        delete connections[key].mobile; // Removing mobile from connections as it is disconnected
+        break;
+      }
+    }
+});
+
 
   socket.on('button-pressed', ({ key, shortcut }) => {
     if (connections[key] && connections[key].desktop) {
       io.to(connections[key].desktop).emit('button-pressed', shortcut);
     }
-  });  
+  });
 });
 
 function generateKey() {
--- WebApp/src/app.js
+++ WebApp/src/app.js
@@ -1,38 +1,47 @@
 const socket = io.connect('http://136.57.134.145:80');
 const connectionKeyInput = document.getElementById('connection-key');
 const statusElement = document.getElementById('status');
+const connectedContainer = document.getElementById('connected-container');
+const connectionContainer = document.getElementById('connection-container');
+const disconnectButton = document.getElementById('disconnect-button');
 
 function connect() {
     const key = connectionKeyInput.value;
     if (key.length === 6) {
-        socket.emit('mobile-connect', key);
+    socket.emit('mobile-connect', key);
     } else {
-        statusElement.textContent = 'Please enter a 6-digit key.';
+    statusElement.textContent = 'Please enter a 6-digit key.';
     }
+}
+
+function buttonPressed(shortcut) {
+    const key = connectionKeyInput.value;
+    socket.emit('button-pressed', { key, shortcut });
+}
+
+function disconnect() {
+    socket.emit('mobile-disconnect');
+    showDisconnectedStatus();
 }
 
 socket.on('connection-error', (message) => {
     statusElement.textContent = message;
 });
 
-
-function buttonPressed() {
-    const key = connectionKeyInput.value;
-    socket.emit('button-pressed', key);
-}
-
-function buttonPressed(shortcut) {
-    const key = connectionKeyInput.value;
-    socket.emit('button-pressed', {
-        key,
-        shortcut
-    });
-}
-
-
-
 socket.on('mobile-connected', () => {
     statusElement.textContent = 'Connected to desktop!';
-    console.log("HI");
-    document.getElementById('connected-container').style.display = 'block';
+    connectedContainer.style.display = 'block';
+    connectionContainer.style.display = 'none';
+    disconnectButton.classList.remove('hidden');
 });
+
+socket.on('desktop-disconnected', () => {
+    showDisconnectedStatus();
+});
+
+function showDisconnectedStatus() {
+    connectedContainer.style.display = 'none';
+    connectionContainer.style.display = 'block';
+    disconnectButton.classList.add('hidden');
+    statusElement.textContent = 'Disconnected.';
+}
